411f2b75940f494ba3bd6c8372845afc1d9987b9
Alberto Minetti <alberto.minetti@gmail.com>
2023-12-05 00:50:36 +0100

    day04 for AoC 2023 (part 1)
    Took 32 minutes

---
+++ src/main/java/aminetti/adventofcode2023/day04/PileOfScratchCards.java
@@ -0,0 +1,57 @@
+package aminetti.adventofcode2023.day04;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.String.join;
+import static java.util.Collections.nCopies;
+
+public class PileOfScratchCards {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PileOfScratchCards.class);
+
+    public int calculatePoints(List<String> lines, int winningNumbers, int playedNumbers) {
+
+        String digits = "(\\d+)";
+        String winning = join(" +", nCopies(winningNumbers, digits));
+        String played = join(" +", nCopies(playedNumbers, digits));
+
+        String regex = "Card +\\d+: +%s \\| +%s".formatted(winning, played);
+        LOGGER.info("Regex {}", regex);
+
+        Pattern pattern = Pattern.compile(regex);
+
+        int score = 0;
+        for (String line : lines) {
+            Matcher matcher = pattern.matcher(line);
+
+            if (matcher.find()) {
+                Set<Integer> winningNums = new HashSet<>();
+                int i = 1;
+                for (; i <= winningNumbers; i++) {
+                    LOGGER.debug("Match: {}", matcher.group(i));
+                    winningNums.add(parseInt(matcher.group(i)));
+                }
+                Set<Integer> playedNums = new HashSet<>();
+                for (; i <= winningNumbers + playedNumbers; i++) {
+                    LOGGER.debug("Match: {}", matcher.group(i));
+                    playedNums.add(parseInt(matcher.group(i)));
+                }
+                Collection<Integer> wins = CollectionUtils.intersection(winningNums, playedNums);
+
+                score += (int) Math.pow(2, wins.size() - 1);
+
+            }
+        }
+
+        return score;
+    }
+}
---
+++ src/test/java/aminetti/adventofcode2023/day04/PileOfScratchCardsTest.java
@@ -0,0 +1,39 @@
+package aminetti.adventofcode2023.day04;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.commons.io.IOUtils.readLines;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class PileOfScratchCardsTest {
+    PileOfScratchCards solver = new PileOfScratchCards();
+
+    @Test
+    void getPoints() {
+        int points = solver.calculatePoints(List.of(
+                        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+                        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+                        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+                        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+                        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+                        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"),
+                5, 8
+        );
+
+        assertThat(points, is(13));
+    }
+
+
+    @Test
+    void getPointsForRealInputPart1() {
+        List<String> list = readLines(PileOfScratchCardsTest.class.getResourceAsStream("/day04/day4_input.txt"), UTF_8);
+
+        int points = solver.calculatePoints(list, 10, 25);
+
+        assertThat(points, is(21568));
+    }
+}
