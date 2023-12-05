416c11ad56a3e19b27dc3021cb3ae6babff19f12
Alberto Minetti <alberto.minetti@gmail.com>
2023-12-05 01:23:50 +0100

    day04 for AoC 2023 (part 2)
    Took 28 minutes

--- src/main/java/aminetti/adventofcode2023/day04/PileOfScratchCards.java
+++ src/main/java/aminetti/adventofcode2023/day04/PileOfScratchCards.java
@@ -4,10 +4,7 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -36,8 +33,6 @@ public class PileOfScratchCards {
     }
 
     public int calculatePoints(List<String> lines) {
-
-
         int score = 0;
         for (String line : lines) {
             Matcher matcher = pattern.matcher(line);
@@ -62,6 +57,49 @@ public class PileOfScratchCards {
         }
 
         return score;
+    }
+
+    public int calculateWinningCards(List<String> lines) {
+        HashMap<Integer, Integer> winnedCards = new HashMap<>();
+        for (int i = 0; i < lines.size(); i++) {
+            winnedCards.put(i, 1);
+        }
+
+        for (int i = 0; i < lines.size(); i++) {
+            int nextCards = calculateCards(lines, i);
+            int instancesOfThisCard = winnedCards.get(i);
+            for (int j = 1; j <= nextCards && i + j < lines.size(); j++) {
+                winnedCards.compute(i + j, (k, v) -> v + instancesOfThisCard);
+            }
+        }
+
+        return winnedCards.values().stream().mapToInt(v -> v).sum();
+    }
+
+    public int calculateCards(List<String> lines, int lineNum) {
+        int nextCards = 0;
+        String line = lines.get(lineNum);
+        Matcher matcher = pattern.matcher(line);
+
+        if (matcher.find()) {
+            Set<Integer> winningNums = new HashSet<>();
+            int i = 1;
+            for (; i <= winningNumbers; i++) {
+                LOGGER.debug("Match: {}", matcher.group(i));
+                winningNums.add(parseInt(matcher.group(i)));
+            }
+            Set<Integer> playedNums = new HashSet<>();
+            for (; i <= winningNumbers + playedNumbers; i++) {
+                LOGGER.debug("Match: {}", matcher.group(i));
+                playedNums.add(parseInt(matcher.group(i)));
+            }
+            Collection<Integer> wins = CollectionUtils.intersection(winningNums, playedNums);
+
+            nextCards += wins.size();
+
+        }
+
+        return nextCards;
     }
 
 
