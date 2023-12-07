80d3ecdfa4121ab22374edcf4d00a641348a3277
Alberto Minetti <alberto.minetti@gmail.com>
2023-12-07 11:00:05 +0100

    day07 for AoC 2023 (part 2)
    Took 23 minutes

--- src/main/java/aminetti/adventofcode2023/day07/CamelCards.java
+++ src/main/java/aminetti/adventofcode2023/day07/CamelCards.java
@@ -4,16 +4,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.StringJoiner;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static aminetti.adventofcode2023.day07.CamelCards.Hand.Kind.*;
+import static aminetti.adventofcode2023.day07.CamelCards.Kind.*;
 import static java.util.Comparator.reverseOrder;
 import static java.util.stream.Collectors.*;
 
 public class CamelCards {
     private static final Logger LOGGER = LoggerFactory.getLogger(CamelCards.class);
+
+
+    public enum Kind {
+        HIGH_CARD, ONE_PAIR, TWO_PAIRS, THREE_OF_K, FULL_HOUSE, FOUR_OF_K, FIVE_OF_K
+    }
 
     public Hand readCardAndBet(String s) {
         Pattern regex = Pattern.compile("(.{5}) (\\d+)"); // trusting the input
@@ -25,6 +31,10 @@ public class CamelCards {
         }
 
         throw new IllegalArgumentException("Wrong format for hand!");
+    }
+
+    public HandWithJoker toHandWithJoker(Hand h) {
+        return new HandWithJoker(h.cards, h.bet);
     }
 
     public long solve(List<String> lines) {
@@ -46,11 +56,28 @@ public class CamelCards {
         return totalWinning;
     }
 
+    public long solvePart2(List<String> lines) {
+        List<HandWithJoker> reversedRankedHands = lines.stream()
+                .map(this::readCardAndBet)
+                .map(this::toHandWithJoker)
+                .sorted()
+                .toList();
+
+        int totalWinning = 0; // too much duplication :-(
+
+        int rank = 1;
+        for (HandWithJoker rankedHand : reversedRankedHands) {
+            int score = rank * rankedHand.bet;
+            LOGGER.info("Hand {}, revRank {}, score {}", rankedHand, rank, score);
+            totalWinning += score;
+            rank++;
+        }
+
+        return totalWinning;
+    }
+
     public record Hand(String cards, int bet) implements Comparable<Hand> {
 
-        public enum Kind {
-            HIGH_CARD, ONE_PAIR, TWO_PAIRS, THREE_OF_K, FULL_HOUSE, FOUR_OF_K, FIVE_OF_K
-        }
 
         public Kind kind() {
             List<Long> bestGroups = cards.chars()
@@ -59,11 +86,11 @@ public class CamelCards {
                     .values().stream().sorted(reverseOrder())
                     .toList();
 
-            Long bestGroup = bestGroups.get(0);
+            long bestGroup = bestGroups.get(0);
             if (bestGroup == 5) return FIVE_OF_K;
             if (bestGroup == 4) return FOUR_OF_K;
 
-            Long secondBestGroup = bestGroups.get(1);
+            long secondBestGroup = bestGroups.get(1);
             if (bestGroup == 3) {
                 return secondBestGroup == 2 ? FULL_HOUSE : THREE_OF_K;
             }
@@ -101,4 +128,77 @@ public class CamelCards {
             };
         }
     }
+
+    public record HandWithJoker(String cards, int bet) implements Comparable<HandWithJoker> {
+
+        public Kind kind() {
+            long jokerCount = cards.chars().filter(HandWithJoker::isJoker).count();
+
+            if (jokerCount == 5) return FIVE_OF_K;
+
+            List<Long> bestGroups = cards.chars()
+                    .filter(c -> !isJoker(c))
+                    .boxed()
+                    .collect(groupingBy(Function.identity(), counting()))
+                    .values().stream().sorted(reverseOrder())
+                    .toList();
+
+            if (bestGroups.isEmpty()) return HIGH_CARD;
+
+            long bestGroup = bestGroups.get(0) + jokerCount;
+            if (bestGroup == 5) return FIVE_OF_K;
+            if (bestGroup == 4) return FOUR_OF_K;
+
+            long secondBestGroup = bestGroups.get(1);
+            if (bestGroup == 3) {
+                return secondBestGroup == 2 ? FULL_HOUSE : THREE_OF_K;
+            }
+            if (bestGroup == 2) {
+                return secondBestGroup == 2 ? TWO_PAIRS : ONE_PAIR;
+            }
+            return HIGH_CARD;
+        }
+
+        private static boolean isJoker(int c) {
+            return c == 'J';
+        }
+
+        @Override
+        public int compareTo(HandWithJoker o) {
+            int compareByKind = this.kind().compareTo(o.kind());
+            if (compareByKind != 0) return compareByKind; // different kind
+
+
+            int compare = 0;
+            for (int i = 0; i < 5; i++) {
+                compare = Integer.compare(
+                        toLabelValue(this.cards.charAt(i)),
+                        toLabelValue(o.cards.charAt(i))
+                );
+                if (compare != 0) break;
+            }
+            return compare;
+        }
+
+        @Override
+        public String toString() {
+            return new StringJoiner(", ", HandWithJoker.class.getSimpleName() + "[", "]")
+                    .add("cards='" + cards + "'")
+                    .add("kind='" + kind() + "'")
+                    .add("bet=" + bet)
+                    .toString();
+        }
+
+        public static int toLabelValue(char c) {
+            return switch (c) {
+                case 'T' -> 10;
+                case 'J' -> 1; // joker is weakest
+                case 'Q' -> 12;
+                case 'K' -> 13;
+                case 'A' -> 14;
+                default -> Character.getNumericValue(c);
+            };
+        }
+    }
+
 }
--- src/test/java/aminetti/adventofcode2023/day07/CamelCardsTest.java
+++ src/test/java/aminetti/adventofcode2023/day07/CamelCardsTest.java
@@ -1,11 +1,12 @@
 package aminetti.adventofcode2023.day07;
 
 import aminetti.adventofcode2023.day07.CamelCards.Hand;
+import aminetti.adventofcode2023.day07.CamelCards.HandWithJoker;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static aminetti.adventofcode2023.day07.CamelCards.Hand.Kind.*;
+import static aminetti.adventofcode2023.day07.CamelCards.Kind.*;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.apache.commons.io.IOUtils.readLines;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -72,6 +73,67 @@ class CamelCardsTest {
         long marginError = solver.solve(list);
 
         assertThat(marginError, is((long) 251106089));
+    }
+
+    // part 2 with joker
+
+
+    @Test
+    void solveTestInputPart2() {
+        long totalWining = solver.solvePart2(
+                List.of(
+                        "32T3K 765",
+                        "T55J5 684",
+                        "KK677 28",
+                        "KTJJT 220",
+                        "QQQJA 483"
+                ));
+
+        assertThat(totalWining, is((long) 5905));
+    }
+
+
+    @Test
+    void testKindCalculationPart2() {
+        assertThat(new HandWithJoker("32T3K", 765).kind(), is(ONE_PAIR));
+        assertThat(new HandWithJoker("KK677", 684).kind(), is(TWO_PAIRS));
+        assertThat(new HandWithJoker("KTJJT", 28).kind(), is(FOUR_OF_K));
+        assertThat(new HandWithJoker("QQQJA", 220).kind(), is(FOUR_OF_K));
+        assertThat(new HandWithJoker("AKAKA", 483).kind(), is(FULL_HOUSE));
+        assertThat(new HandWithJoker("32165", 1).kind(), is(HIGH_CARD));
+        assertThat(new HandWithJoker("35555", 1).kind(), is(FOUR_OF_K));
+        assertThat(new HandWithJoker("11111", 2).kind(), is(FIVE_OF_K));
+        assertThat(new HandWithJoker("JJJJ1", 2).kind(), is(FIVE_OF_K));
+    }
+
+    @Test
+    void testComparatorBasedOnKindOnlyPart2() {
+        assertThat(new HandWithJoker("11111", 2), greaterThan(new HandWithJoker("32165", 1)));
+        assertThat(new HandWithJoker("KK677", 684), greaterThan(new HandWithJoker("32T3K", 765)));
+        assertThat(new HandWithJoker("QQQJA", 220), greaterThan(new HandWithJoker("32T3K", 765)));
+        assertThat(new HandWithJoker("QQQJA", 220), greaterThan(new HandWithJoker("35555", 1)));
+    }
+
+    @Test
+    void testComparatorBasedOnKindAndOrderPart2() {
+        assertThat(new HandWithJoker("11111", 2), lessThan(new HandWithJoker("22222", 1)));
+        assertThat(new HandWithJoker("KK677", 684), greaterThan(new HandWithJoker("776KK", 765)));
+        assertThat(new HandWithJoker("QQQJA", 220), greaterThan(new HandWithJoker("AAA96", 765)));
+        assertThat(new HandWithJoker("91111", 220), greaterThan(new HandWithJoker("35555", 1)));
+        assertThat(new HandWithJoker("J1111", 220), lessThan(new HandWithJoker("AAAAA", 1)));
+        assertThat(new HandWithJoker("3J333", 220), greaterThan(new HandWithJoker("2J222", 1)));
+        assertThat(new HandWithJoker("J3333", 220), lessThan(new HandWithJoker("2J222", 1)));
+    }
+
+    @Test
+    void solvePart2() {
+        List<String> list = readLines(CamelCardsTest.class.getResourceAsStream("/day07/day7_input.txt"), UTF_8);
+
+        long marginError = solver.solvePart2(list);
+
+        assertThat(marginError, greaterThan((long) 248209474));
+        assertThat(marginError, greaterThan((long) 248439515));
+        assertThat(marginError, is((long) 249620106));
     }
 
 }
