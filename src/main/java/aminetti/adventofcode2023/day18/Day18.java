c8a5424ce1430a254e47c7890db9ffabea5254b5
Alberto Minetti <alberto.minetti@gmail.com>
2024-01-25 11:54:33 +0100

    day18 for AoC 2023 (part 2)
    Took 1 hour 12 minutes

--- src/main/java/aminetti/adventofcode2023/day18/Day18.java
+++ src/main/java/aminetti/adventofcode2023/day18/Day18.java
@@ -3,15 +3,16 @@ package aminetti.adventofcode2023.day18;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.StringJoiner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
-import static aminetti.adventofcode2023.day18.Day18.Direction.*;
+import static aminetti.adventofcode2023.day18.Day18.Direction.D;
 import static aminetti.adventofcode2023.day18.Day18.Direction.U;
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.*;
 import static java.util.stream.Collectors.toList;
 
 public class Day18 {
@@ -21,25 +22,47 @@ public class Day18 {
 
 
     public Day18(List<String> input) {
-        this.instr = parseInstructions(input);
-        prepareMinsAndMaxs();
+        this(input, false);
     }
 
-    private List<Instruction> parseInstructions(List<String> plainInstructions) {
+    public Day18(List<String> input, boolean part2) {
+        this.instr = part2 ? parseInstructionsPart2(input) : parseInstructions(input);
+    }
+
+    protected List<Instruction> parseInstructions(List<String> plainInstructions) {
         Pattern pattern = Pattern.compile("(.) (\\d+) \\((\\S+)\\)");
         return plainInstructions.stream().map(s -> {
             Matcher matcher = pattern.matcher(s);
             if (matcher.matches()) {
-                return new Instruction(Direction.valueOf(matcher.group(1)), Integer.parseInt(matcher.group(2)), matcher.group(3));
+                return new Instruction(Direction.valueOf(matcher.group(1)), parseInt(matcher.group(2)));
             }
             throw new IllegalArgumentException("Something wrong with your input, or with your parsing!");
         }).collect(toList());
     }
 
-    public long solve() {
-        char[][] field = new char[maxY - minY + 3][maxX - minX + 3];
+    protected List<Instruction> parseInstructionsPart2(List<String> plainInstructions) {
+        Pattern pattern = Pattern.compile(". \\d+ \\(#(\\S{5})(\\d)\\)");
+        return plainInstructions.stream().map(s -> {
+            Matcher matcher = pattern.matcher(s);
+            if (matcher.matches()) {
+                String movesHex = matcher.group(1);
+                String directionAsStringOrdinal = matcher.group(2);
+                return new Instruction(Direction.values()[parseInt(directionAsStringOrdinal)], parseInt(movesHex, 16));
+            }
+            throw new IllegalArgumentException("Something wrong with your input, or with your parsing!");
+        }).collect(toList());
+    }
+
+    public long solveWithGrid() {
+
+        prepareMinsAndMaxs();
+
+        int ySize = maxY - minY + 3;
+        int xSize = maxX - minX + 3;
+        LOGGER.info("Trying to initialize a matrix {}x{}", ySize, xSize);
+        boolean[][] field = new boolean[ySize][xSize];
         for (int i = 0; i < field.length; i++) {
-            Arrays.fill(field[i], '.');
+            Arrays.fill(field[i], false);
         }
 
         int xCursor = -minX + 1;
@@ -56,7 +79,7 @@ public class Day18 {
                     case U -> yCursor--;
                     case D -> yCursor++;
                 }
-                field[yCursor][xCursor] = '#';
+                field[yCursor][xCursor] = true;
             }
         }
         LOGGER.info("Cursor {},{}", xCursor, yCursor);
@@ -65,9 +88,9 @@ public class Day18 {
         show(field);
 
 
-        char[][] areaFilled = new char[maxY - minY + 3][maxX - minX + 3];
+        boolean[][] areaFilled = new boolean[ySize][maxX - minX + 3];
         for (int i = 0; i < areaFilled.length; i++) {
-            Arrays.fill(areaFilled[i], '.');
+            Arrays.fill(areaFilled[i], false);
         }
         long areaAndTrench = 0L;
         for (int i = 0; i < field.length; i++) {
@@ -77,22 +100,22 @@ public class Day18 {
             Direction crossingFrom = null;
             for (int j = 0; j < field[i].length; j++) {
 
-                if (field[i][j] == '#') {
-                    if (field[i + 1][j] == '#' && field[i - 1][j] == '#') { // vertical trench
+                if (field[i][j]) {
+                    if (field[i + 1][j] && field[i - 1][j]) { // vertical trench
                         crossingTimes++;
-                    } else if (field[i + 1][j] != '#' && field[i - 1][j] != '#') { // horizontal trench
+                    } else if (!field[i + 1][j] && !field[i - 1][j]) { // horizontal trench
                         assert crossingFrom != null;
                     } else {
                         if (crossingFrom == null) {
-                            if (field[i - 1][j] == '#') {
+                            if (field[i - 1][j]) {
                                 crossingFrom = U;
-                            } else if (field[i + 1][j] == '#') {
+                            } else if (field[i + 1][j]) {
                                 crossingFrom = D;
                             }
                         } else {
-                            if (field[i - 1][j] == '#' && crossingFrom == D) {
+                            if (field[i - 1][j] && crossingFrom == D) {
                                 crossingTimes++;
-                            } else if (field[i + 1][j] == '#' && crossingFrom == U) {
+                            } else if (field[i + 1][j] && crossingFrom == U) {
                                 crossingTimes++;
                             }
                             crossingFrom = null;
@@ -103,10 +126,10 @@ public class Day18 {
                     }
 
                     areaAndTrench++;
-                    areaFilled[i][j] = '#';
+                    areaFilled[i][j] = true;
                 } else if (crossingTimes % 2 != 0) {
                     areaAndTrench++;
-                    areaFilled[i][j] = '#';
+                    areaFilled[i][j] = true;
                 }
 
             }
@@ -119,11 +142,11 @@ public class Day18 {
         return areaAndTrench;
     }
 
-    private void show(char[][] field) {
+    private void show(boolean[][] field) {
         System.out.println("---");
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++) {
-                System.out.print(field[i][j]);
+                System.out.print(field[i][j] ? '#' : '.');
             }
             System.out.println();
         }
@@ -141,8 +164,8 @@ public class Day18 {
             };
 
             xCursor += xMove;
-            minX = Math.min(minX, xCursor);
-            maxX = Math.max(maxX, xCursor);
+            minX = min(minX, xCursor);
+            maxX = max(maxX, xCursor);
 
             int yMove = switch (i.d) {
                 case U -> i.moves * -1;
@@ -151,8 +174,8 @@ public class Day18 {
             };
 
             yCursor += yMove;
-            minY = Math.min(minY, yCursor);
-            maxY = Math.max(maxY, yCursor);
+            minY = min(minY, yCursor);
+            maxY = max(maxY, yCursor);
 
         }
 
@@ -160,14 +183,83 @@ public class Day18 {
     }
 
 
-    public record Instruction(Direction d, int moves, String color) {
+    public long solveWithPick() {
+        List<Point> points = prepareVertices();
+        System.out.println(points);
+
+        // By Pick theorem https://www.wikiwand.com/en/Pick%27s_theorem
+        // Area in lattice = internal points + (boundary points)/2 - 1
+
+
+        // => internal points = Area - (boundary points)/2 + 1
+
+        long perimeter = instr.stream().mapToLong(i -> i.moves).sum();
+        long area = (long) shoelaceArea(points);
+        long innerPoints = area - (perimeter / 2) + 1;
+
+        // => internal points + boundary points = Area + (boundary points)/2 + 1
+
+        long areaAndBoundary = innerPoints + perimeter;
+
+        LOGGER.info("Perimeter {}; Area {}; Internal points {}; total {}", perimeter, area, innerPoints, areaAndBoundary);
+        return areaAndBoundary;
+    }
+
+    private List<Point> prepareVertices() {
+        List<Point> points = new ArrayList<>();
+        int xCursor = 0;
+        int yCursor = 0;
+        points.add(new Point(xCursor, yCursor));
+        for (Instruction i : instr) {
+            int xMove = switch (i.d) {
+                case L -> i.moves * -1;
+                case R -> i.moves;
+                default -> 0;
+            };
+            xCursor += xMove;
+
+            int yMove = switch (i.d) {
+                case U -> i.moves * -1;
+                case D -> i.moves;
+                default -> 0;
+            };
+
+            yCursor += yMove;
+
+            points.add(new Point(xCursor, yCursor));
+        }
+
+        return points;
+    }
+
+
+    public record Instruction(Direction d, int moves) {
         @Override
         public String toString() {
-            return "%s %d %s".formatted(d, moves, color);
+            return "%s %d".formatted(d, moves);
         }
     }
 
     public enum Direction {
-        U, D, L, R
+        R, D, L, U
+    }
+
+    public static double shoelaceArea(List<Point> vertices) {
+        // https://www.wikiwand.com/en/Shoelace_formula
+        double area = 0L;
+        for (int i = 0; i < vertices.size() - 1; i++) {
+            Point p1 = vertices.get(i);
+            Point p2 = vertices.get(i + 1);
+
+            area += p1.x * p2.y - p1.y * p2.x;
+        }
+        area += vertices.getLast().x * vertices.getFirst().y - vertices.getLast().y * vertices.getFirst().x;
+
+        return abs(area) / 2;
+    }
+
+
+    record Point(long x, long y) {
+
     }
 }
--- src/test/java/aminetti/adventofcode2023/day18/Day18Test.java
+++ src/test/java/aminetti/adventofcode2023/day18/Day18Test.java
@@ -18,7 +18,7 @@ class Day18Test {
         List<String> list = readLines(Day18Test.class.getResourceAsStream("/day18/day18_test_input.txt"), UTF_8);
 
         Day18 solver = new Day18(list);
-        long l = solver.solve();
+        long l = solver.solveWithGrid();
 
         assertThat(l, is(62L));
     }
@@ -28,10 +28,52 @@ class Day18Test {
         List<String> list = readLines(Day18Test.class.getResourceAsStream("/day18/day18_input.txt"), UTF_8);
 
         Day18 solver = new Day18(list);
-        long l = solver.solve();
+        long l = solver.solveWithGrid();
 
         assertThat(l, is(greaterThan(76311L)));
-        assertThat(l, is(76311L));
+        assertThat(l, is(76387L));
+    }
+
+
+    @Test
+    void testInputPart1WithPick() {
+        List<String> list = readLines(Day18Test.class.getResourceAsStream("/day18/day18_test_input.txt"), UTF_8);
+
+        Day18 solver = new Day18(list);
+        long l = solver.solveWithPick();
+
+        assertThat(l, is(62L));
+    }
+
+    @Test
+    void testActualInputPart1WithPick() {
+        List<String> list = readLines(Day18Test.class.getResourceAsStream("/day18/day18_input.txt"), UTF_8);
+
+        Day18 solver = new Day18(list);
+        long l = solver.solveWithPick();
+
+        assertThat(l, is(76387L));
+    }
+
+
+    @Test
+    void testInputPart2() {
+        List<String> list = readLines(Day18Test.class.getResourceAsStream("/day18/day18_test_input.txt"), UTF_8);
+
+        Day18 solver = new Day18(list, true);
+        long l = solver.solveWithPick();
+
+        assertThat(l, is(952408144115L));
+    }
+
+    @Test
+    void testActualInputPart2() {
+        List<String> list = readLines(Day18Test.class.getResourceAsStream("/day18/day18_input.txt"), UTF_8);
+
+        Day18 solver = new Day18(list, true);
+        long l = solver.solveWithPick();
+
+        assertThat(l, is(250022188522074L));
     }
 
 }
